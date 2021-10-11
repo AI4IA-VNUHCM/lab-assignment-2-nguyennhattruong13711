132d979c8f90539d2a244e537bb3b18bc5b06154
nguyennhattruong13711 <91869002+nguyennhattruong13711@users.noreply.github.com>
2021-10-11 15:08:15 +0700

    update Ex2

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -13,24 +13,18 @@ ______________________________________
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int n = atoi(argv[1]);
 	int result;
-	int result = 1;
 	//Your codes here
+	result = 1;
+	int i = n % 2 == 0 ? 2 : 1;
+	for (; i <= n; i += 2)
+	{
+		result *= i;
+	}
 
-	if(n % 2 != 0){
-		for(int i = 1; i <= n; i += 2){
-			result *= i;
-		}
-	}
-	else{
-		for(int i = 2; i <= n; i += 2){
-			result *= i;
-		}
-	}
 
 	printf("%d", result);
 	return 0;
